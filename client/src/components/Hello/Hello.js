e539d0a9d1d2a9dd27aeafdd6ca947ac1886e359
Danilo Bräuer <danilobrauer@Danilos-MacBook-Pro.local>
2021-10-25 09:48:48 +0200

    nothing super relevant, tried out more transparent Klammerfischlogo

--- client/src/components/Hello/Hello.js
+++ client/src/components/Hello/Hello.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/macro'
-import Klammerfisch from '../../assets/images/Klammerfisch.jpg'
+import Klammerfisch from '../../assets/images/Klammerfisch_frei.png'
 
 function Hello() {
   return (
